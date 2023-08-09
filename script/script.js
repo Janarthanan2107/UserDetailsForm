a35806a966fbac66c0d661a03864e013aeb52d1b
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-08-10 00:10:14 +0530

    User details ui created

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// Using javascript file as strict file for exposing all the loss errors or eliminating the silent errors into throwable errors
+'use strict';
+
+//selecting every elements from the html files in two types inputs(providing values) and outputs(displaying)
+//inputs
+
+//outputs
+
+//functions
+const init = ()=> {}
+
+//events
+
+//initial settings
+init();
